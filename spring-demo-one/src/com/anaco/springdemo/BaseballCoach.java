ee7874e5a12f733279116744c1cee3480148c1d9
Ana Luiza Sanches <analuiza@42codelab.com>
2021-04-14 18:45:17 -0300

    Inicia projeto demo de spring

---
+++ spring-demo-one/src/com/anaco/springdemo/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.anaco.springdemo;
+
+public class BaseballCoach implements Coach {
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting practice";
+	}
+}
---
+++ spring-demo-one/src/com/anaco/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.anaco.springdemo;
+
+public interface Coach {
+
+	String getDailyWorkout();
+
+}
