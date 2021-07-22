c4c7538892602f5a22d1ae3864ab3dc3e5454ddf
Ana Luiza Sanches <analuizatrz@gmail.com>
2021-07-22 11:38:39 -0300

    conecting to database

---
+++ hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,22 @@
+package com.luv2code.jdbc;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJdbc {
+	public static void main(String[] args) {
+		var url = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&characterEncoding=latin1";
+		var username = "hbstudent";
+		var password = "hbstudent";
+
+		System.out.println("Connecting database...");
+		try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver()); //Essa linha foi a diferença
+		    System.out.print("Connnecting to database");
+			var conn = DriverManager.getConnection(url, username, password);
+			System.out.println("Connection sucessfull !");
+		} catch (SQLException e) {
+		    throw new IllegalStateException("Cannot connect the database!", e);
+		}
+	}
+}
