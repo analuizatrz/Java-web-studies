db38143435dcf6c64bf3fbf798f42c7a6b47132a
Ana Luiza Sanches <analuizatrz@gmail.com>
2021-07-22 00:45:01 -0300

    validation with regex pattern

--- spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -15,6 +16,9 @@ public class Customer {
 	@Min(value=0, message="must be greater or equal to 0")
 	@Max(value=10, message="must be lower or equal to 10")
 	private int freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
 	
 
 	public String getFirstName() {
@@ -39,6 +43,14 @@ public class Customer {
 
 	public void setFreePasses(int freePasses) {
 		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 	
 }
--- spring-mvc-demo/src/main/webapp/WEB-INF/view/customer-confirmation.jsp
+++ spring-mvc-demo/src/main/webapp/WEB-INF/view/customer-confirmation.jsp
@@ -8,6 +8,9 @@
 		The customer  is confirmed: ${customer.firstName} ${customer.lastName}
 		<br><br>
 		Free Passes: ${customer.freePasses }
+		
+		<br><br>
+		Postal Code: ${customer.postalCode}
 
 	</body>
 	
