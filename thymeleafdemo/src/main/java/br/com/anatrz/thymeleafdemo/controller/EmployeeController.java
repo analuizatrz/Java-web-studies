121aef1c414ef003068743bade713fd2a5a4c86b
Ana Luiza Sanches <analuizatrz@gmail.com>
2021-08-07 12:26:22 -0300

    add employee thymeleaf

--- thymeleafdemo/src/main/java/br/com/anatrz/thymeleafdemo/controller/EmployeeController.java
+++ thymeleafdemo/src/main/java/br/com/anatrz/thymeleafdemo/controller/EmployeeController.java
@@ -1,13 +1,12 @@
 package br.com.anatrz.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.anatrz.thymeleafdemo.entity.Employee;
@@ -34,7 +33,24 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeService.save(employee);
+		return "redirect:employees/list";
 	}
 }
 
